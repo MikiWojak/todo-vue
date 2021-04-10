211925b155f6146754d2af3620b067f483eadca3
MikiWojak <mikizarowa8@gmail.com>
2021-04-10 17:43:00 +0200

    Interface of TODO list
    - Add an input for the todo
    - Add list of predefined todos

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,5 +1,11 @@
 <template>
-    <div>TodoList goes here</div>
+  <div>
+    <input type="text" class="todo-input" placeholder="What needs to be done" v-model="newTodo">
+
+    <div v-for="todo in todos" :key="todo.id" class="todo-item">
+      {{ todo.title }}
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,11 +13,33 @@ export default {
   name: 'todo-list',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+     newTodo: '',
+     todos: [
+       {
+        'id': 1,
+        'title': 'Finish Vue Screencast',
+        'completed': false
+       },
+       {
+        'id': 2,
+        'title': 'Take over world',
+        'completed': false
+       },
+     ]
     }
   }
 }
 </script>
 
 <style>
+  .todo-input {
+    width: 100%;
+    padding: 10px 18px;
+    font-size: 18px;
+    margin-bottom: 16px;
+  }
+
+  .todo-input:focus {
+    outline: 0;
+  }
 </style>
