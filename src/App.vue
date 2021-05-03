5abef79aca25a864a5494c7409502cfa4510aa66
MikiWojak <mikizarowa8@gmail.com>
2021-05-03 16:42:48 +0200

    Prepare Components for routing
    - Add some Vue Components
    - Prepare for Vue Routing
    - Some Components are null at the moment

--- src/App.vue
+++ src/App.vue
@@ -27,16 +27,17 @@ body {
   max-width: 600px;
   margin: 0 auto;
 }
-
+/* 
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
   font-size: 24px;
-}
+
+  text-align: center;
+} */
 
 .logo {
   display: block;
