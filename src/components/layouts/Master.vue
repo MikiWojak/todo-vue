8af41144795cfc128f5a9afa2af7f0a9886d70ca
MikiWojak <mikizarowa8@gmail.com>
2021-05-07 14:15:00 +0200

    Assign links to buttons
    - Assign the links to the menu bar

--- src/components/layouts/Master.vue
+++ src/components/layouts/Master.vue
@@ -1,10 +1,11 @@
 <template>
     <div id="app">
         <ul class="nav">
-            <li><a href="/">Home</a></li>
-            <li><a href="/">About</a></li>
-            <li><a href="/">Login</a></li>
-            <li><a href="/">Register</a></li>
+            <li><router-link to="/">Home</router-link></li>
+            <li><router-link to="/todo">App</router-link></li>
+            <li><router-link to="/about">About</router-link></li>
+            <li><router-link to="/login">Login</router-link></li>
+            <li><router-link to="/register">Register</router-link></li>
         </ul>
 
         <router-view></router-view>
