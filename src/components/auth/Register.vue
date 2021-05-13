3960f7927b06a745fabb021ee24e1545cacaf394
MikiWojak <mikizarowa8@gmail.com>
2021-05-13 10:47:41 +0200

    Login and register form
    - Code login and register forms
    - Style them in the Master component

--- src/components/auth/Register.vue
+++ src/components/auth/Register.vue
@@ -1,5 +1,28 @@
+
 <template>
-    <div class="flex-center">
-        Content for Register Page
+    <div class="login-form">
+        <h2 class="login-heading">Register</h2>
+        <form action="#">
+
+        <div class="form-control">
+            <label for="name">Name</label>
+            <input type="text" name="name" id="name" class="login-input">
+        </div>
+
+        <div class="form-control">
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" class="login-input">
+        </div>
+
+        <div class="form-control mb-more">
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" class="login-input">
+        </div>
+
+        <div class="form-control">
+            <button type="submit" class="btn-submit">Create Account</button>
+        </div>
+
+        </form>
     </div>
 </template>
